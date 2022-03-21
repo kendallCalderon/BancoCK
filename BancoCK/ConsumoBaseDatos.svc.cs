74468475d8ad900043d1231621fd864cf49bd505
cristian <camilorestrepo@outlook.es>
2022-03-21 14:42:45 -0600

    Archivos frecuentes desde la rama respaldo

--- BancoCK/ConsumoBaseDatos.svc.cs
+++ BancoCK/ConsumoBaseDatos.svc.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
 
 namespace BancoCK
 {
