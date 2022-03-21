2ce0b0fe48e44f16b28b938dc86d26a019089be4
cristian <camilorestrepo@outlook.es>
2022-03-21 00:29:26 -0600

    Web services

---
+++ BancoCK/ConsumoBaseDatos.svc.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BancoCK
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ConsumoBaseDatos" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ConsumoBaseDatos.svc o ConsumoBaseDatos.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ConsumoBaseDatos : IConsumoBaseDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataAdapter adaptador;
+        DataTable DatatableUsuarios = null;
+
+        public void abrirConexion()
+        {
+            try
+            {
+                string strConexion = "Password=Morado571998Medellin;Persist Security Info=True;User ID=proyectoWebGrupo9;Initial Catalog=proyectoWebGrupo9;Data Source=tiusr2pl.cuc-carrera-ti.ac.cr\\MSSQLSERVER2019";
+                conexion = new SqlConnection(strConexion);
+                conexion.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al abrir la conexion con la base de datos, detalles: " + ex.Message);
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            conexion.Close();
+        }
+
+        public void guardarInformacionClienteNoAutenticado(string cedula, string nombre, string apellido1, string apellido2, string correo, int telefono, float salarioNeto, int añosLaborando, float salarioBruto, string rol)
+        {
+            try
+            {
+                abrirConexion();
+                comando = new SqlCommand("almacenarInformacionClienteNoLogeado", conexion);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@Identificacion", cedula);
+                comando.Parameters.AddWithValue("@Nombre", nombre);
+                comando.Parameters.AddWithValue("@Apellido1", apellido1);
+                comando.Parameters.AddWithValue("@Apellido2", apellido2);
+                comando.Parameters.AddWithValue("@Correo", correo);
+                comando.Parameters.AddWithValue("@Telefono", telefono);
+                comando.Parameters.AddWithValue("@SalarioNeto", salarioNeto);
+                comando.Parameters.AddWithValue("@AñosLaborando", añosLaborando);
+                comando.Parameters.AddWithValue("@SalarioBruto", añosLaborando);
+                comando.Parameters.AddWithValue("@Rol", rol);
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocurrio un error al guardar los datos del cliente en BD, detalles: " + ex.Message);
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+        }
+
+        public string mostrarDescripcion(string tipoPrestamo)
+        {
+            try
+            {
+                abrirConexion();
+                comando = new SqlCommand("traerDescripcionPrestamo", conexion);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@tipoPrestamo", tipoPrestamo);
+                adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                DatatableUsuarios = new DataTable();
+                adaptador.Fill(DatatableUsuarios);
+                string usuario = DatatableUsuarios.Rows[0]["Descripcion"].ToString();
+                return usuario;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al recuperar la descripcion del prestamo, detalles:  " + ex.Message);
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+        }
+
+        public string mostrarRequisitos(string tipoPrestamo)
+        {
+            try
+            {
+                abrirConexion();
+                comando = new SqlCommand("traerDescripcionRequisitos", conexion);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@tipoPrestamo", tipoPrestamo);
+                adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                DatatableUsuarios = new DataTable();
+                adaptador.Fill(DatatableUsuarios);
+                string usuario = DatatableUsuarios.Rows[0]["Requisito"].ToString();
+                return usuario;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al recuperar los requisitos del prestamo, detalles:  " + ex.Message);
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+        }
+
+        public int registradoPreviamente(string identificacion)
+        {
+            try
+            {
+                abrirConexion();
+                comando = new SqlCommand("existeUsuario", conexion);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@Identificacion", identificacion);
+                adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                DatatableUsuarios = new DataTable();
+                adaptador.Fill(DatatableUsuarios);
+                if (DatatableUsuarios.Rows.Count > 0)
+                {
+                    return 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al recuperar , detalles:  " + ex.Message);
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+            return 0;
+        }
+
+        public void registrarSolicitudPrestamoUsuarioNoAutenticado(string cedula, string nombre, string apellido1, string apellido2, string correo, int telefono, float salarioNeto, int añosLaborando, float salarioBruto)
+        {
+            try
+            {
+                abrirConexion();
+                comando = new SqlCommand("registrarSolicitudNoAutenticado", conexion);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@Identificacion", cedula);
+                comando.Parameters.AddWithValue("@Nombre", nombre);
+                comando.Parameters.AddWithValue("@Apellido1", apellido1);
+                comando.Parameters.AddWithValue("@Apellido2", apellido2);
+                comando.Parameters.AddWithValue("@Correo", correo);
+                comando.Parameters.AddWithValue("@Telefono", telefono);
+                comando.Parameters.AddWithValue("@SalarioNeto", salarioNeto);
+                comando.Parameters.AddWithValue("@AñosLaborando", añosLaborando);
+                comando.Parameters.AddWithValue("@SalarioBruto", añosLaborando);
+                comando.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocurrio un error al guardar los datos del cliente en BD, detalles: " + ex.Message);
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+
+        }
+
+
+        bool verificarUsuario(string identificacion)
+        {
+            try
+            {
+                abrirConexion();
+                comando = new SqlCommand("existeUsuario", conexion);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@Identificacion", identificacion);
+                adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comando;
+                DatatableUsuarios = new DataTable();
+                adaptador.Fill(DatatableUsuarios);
+                string usuario = DatatableUsuarios.Rows[0]["Identificacion"].ToString();
+                if (usuario.Equals("") || usuario == null)
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al recuperar , detalles:  " + ex.Message);
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+            return true;
+        }
+
+    }
+}
--- BancoCK/pages/FormularioPrestamo.aspx.cs
+++ BancoCK/pages/FormularioPrestamo.aspx.cs
@@ -9,9 +9,45 @@ namespace BancoCK.pages
 {
     public partial class FormularioPrestamo : System.Web.UI.Page
     {
+        ConsumoBaseDatos metodos = new ConsumoBaseDatos();
+        string script = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected void btnTramitar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (metodos.registradoPreviamente(txtIdentificacion.Value.ToString()) == 1)
+                {
+
+                    metodos.registrarSolicitudPrestamoUsuarioNoAutenticado(txtIdentificacion.Value.ToString(), txtNombre.Value.ToString(), txtApellido1.Value.ToString(), txtApellido2.Value.ToString(), txtCorreo.Value.ToString(), int.Parse(txtTelefono.Value.ToString()), float.Parse(txtSalarioNeto.Value.ToString()), int.Parse(txtAñosLaborando.Value.ToString()), float.Parse(txtSalarioBruto.Value.ToString()));
+                    script = string.Format("javascript:notificacion('{0}')", "Se ha enviado tu solicitud de crédito, favor estar atento a tu correo sobre la aprobación de tu credito");
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "notificacion", script, true);
+                }
+                else
+                {
+
+                    metodos.guardarInformacionClienteNoAutenticado(txtIdentificacion.Value.ToString(), txtNombre.Value.ToString(), txtApellido1.Value.ToString(), txtApellido2.Value.ToString(), txtCorreo.Value.ToString(), int.Parse(txtTelefono.Value.ToString()), float.Parse(txtSalarioNeto.Value.ToString()), int.Parse(txtAñosLaborando.Value.ToString()), float.Parse(txtSalarioBruto.Value.ToString()), "Cliente");
+                    script = string.Format("javascript:notificacion('{0}')", "Se ha enviado tu solicitud de crédito, favor estar atento a tu correo sobre la aprobación de tu credito");
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "notificacion", script, true);
+                }
+            }
+            catch (Exception ex)
+            {
+                script = string.Format("javascript:alerta('{0}')", "Error al guardar la informacion del cliente en la BD, favor revisar la bd del sistema bancario");
+
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "alerta", script, true);
+            }
+        }
+
+        protected void btnAtras_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/pages/Tramitador.aspx");
+        }
+
     }
 }
